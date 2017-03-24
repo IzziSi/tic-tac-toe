1994f27ca1cac0870a922196e686794fae3561fa
mvaloris <mvaloris@gmail.com>
2017-03-23 23:20:50 -0600

    edits to html css js

--- scripts/game.js
+++ scripts/game.js
@@ -1,55 +1,64 @@
-console.log("I am working!");
-
-//variables for playing boxes
-var wins = [
-	['#box1', '#box2', '#box3'],
-	['#box4', '#box5', '#box6'],
-	['#box7', '#box8', '#box9'],
-	['#box1', '#box5', '#box9'],
-	['#box1', '#box4', '#box7'],
-	['#box2', '#box5', '#box8'],
-	['#box3', '#box6', '#box9'],
-	['#box3', '#box5', '#box7'],
-];
-
 $(document).ready(function () {
 	console.log("jQuery ready for action!");
 	var currentPlayer = "x";
-	$('.borders').click(function(event) {
+	setTimeout(function() {
+		$('#play-btn').fadeIn(3000);
+	}, 3000);
+	$('.game-board').on('click', '.borders', function() {
 		/*If the current player equals x, on the click of the
 		.borders class the background will change to purple and
 		the current player variable changes to "o"*/
 		if (currentPlayer === "x") {
-			var gryffindor = $(this).append('<img src="images/gryffindor.png">');
+			var gryffindor = $(this).append('<img src="images/gryffindor.png" class="gryffindor">');
 			gryffindor.addClass('gryffindor');
 			$(this).unbind("click");
-			currentPlayer = "o";
+			if (checkWins('gryffindor')) {
+				alert("Gryffindor wins");
+			} else {
+				currentPlayer = "o";	
+			}
+
 		} else {
-			var slytherin = $(this).append('<img src="images/slytherin.png">');
+			var slytherin = $(this).append('<img src="images/slytherin.png" class="slytherin">');
 			slytherin.addClass('slytherin');
 			$(this).unbind("click");
-			currentPlayer = "x";
-		}
+			if (checkWins('slytherin')) {
+				alert("Slytherin wins!");
+			} else {
+				currentPlayer = "x";
+			}
+			
+		}	
 
-		/*$.each(wins, function(index, value) {
-		console.log(wins);
-		if (wins === gryffindor) {
-			console.log("Player 1 wins!");
-		} else if (
-			(wins === ravenclaw)) {
-			console.log("Player 2 wins!");
-		} else (
-			console.log("It's a tie")
-		);
-
-	});*/
-	});
 		$('#reset-btn').click(function(reset) {
-			console.log(reset);
-			console.log(this);
 			$('.gryffindor img').hide();
 			location.reload(true);
 		});
+	});
 
 });
 
+function checkWins(symbol) {
+	if ($('#box1').hasClass(symbol) && $('#box2').hasClass(symbol) && $('#box3').hasClass(symbol)) {
+		return true;
+	} else if ($('#box4').hasClass(symbol) && $('#box5').hasClass(symbol) && $('#box6').hasClass(symbol)) {
+		return true;
+	} else if ($('#box7').hasClass(symbol) && $('#box8').hasClass(symbol) && $('#box9').hasClass(symbol)) {
+		return true;
+	} else if ($('#box1').hasClass(symbol) && $('#box5').hasClass(symbol) && $('#box9').hasClass(symbol)) {
+		return true;
+	} else if ($('#box1').hasClass(symbol) && $('#box4').hasClass(symbol) && $('#box7').hasClass(symbol)) {
+		return true;
+	} else if ($('#box2').hasClass(symbol) && $('#box5').hasClass(symbol) && $('#box8').hasClass(symbol)) {
+		return true;
+	} else if ($('#box3').hasClass(symbol) && $('#box6').hasClass(symbol) && $('#box9').hasClass(symbol)) {
+		return true;
+	} else if ($('#box3').hasClass(symbol) && $('#box5').hasClass(symbol) && $('#box7').hasClass(symbol)) {
+		return true;
+	} else {
+
+	}
+}
+$(function() {
+    $('body').removeClass('fade-out');
+});
---
+++ test.js
@@ -0,0 +1,59 @@
+jesswood [4:37 PM] 
+var turn = 0;
+var tic = 'O';
+var player = 'Player 1';
+
+
+$('.box').click(function() {
+    
+    if($(this).html()==="") {
+        if (turn%2 !== 0) {
+            tic = "X";
+            player = 'Player 2';
+        } else {
+            tic = "O";
+            player = 'Player 1';
+        }
+        $(this).html(tic);
+        checkWin();
+        turn++;
+    }
+
+});
+
+$('#reset').click(function() {
+    $('.box').html('');
+    turn = 0;
+});
+
+function checkWin() {
+    if ($('#b1').html()===tic && $('#b2').html()===tic && $('#b3').html()===tic) {
+        alert(player + " won!");
+
+    } else if ($('#b4').html()===tic && $('#b5').html()===tic && $('#b6').html()===tic) {
+        alert(player + " won!");
+
+    } else if ($('#b7').html()===tic && $('#b8').html()===tic && $('#b9').html()===tic) {
+        alert(player + " won!");
+
+    }    else if ($('#b1').html()===tic && $('#b4').html()===tic && $('#b7').html()===tic) {
+        alert(player + " won!");
+
+    }    else if ($('#b2').html()===tic && $('#b5').html()===tic && $('#b8').html()===tic) {
+        alert(player + " won!");
+
+    }    else if ($('#b3').html()===tic && $('#b6').html()===tic && $('#b9').html()===tic) {
+        alert(player + " won!");
+    
+    }    else if ($('#b3').html()===tic && $('#b5').html()===tic && $('#b7').html()===tic) {
+        alert(player + " won!");
+    
+    }    else if ($('#b1').html()===tic && $('#b5').html()===tic && $('#b9').html()===tic) {
+        alert(player + " won!");
+    // are all boxes checked?
+    }     else if (turn === :sunglasses:  {
+        alert("It's a Tie!");
+    } else {
+
+    }
+}
